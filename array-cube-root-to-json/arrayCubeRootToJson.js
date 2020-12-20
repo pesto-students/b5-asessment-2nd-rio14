e3d7f15027451b8d924c4680cc7385236806b826
rio14 <riteshsinha14.rs@gmail.com>
2020-12-20 22:16:08 +0530

    array cube root completed

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,3 +1,16 @@
-const arrayCubeRootToJson = arr => {};
+const arrayCubeRootToJson = (arr) => {
+  let arrayInJson = {};
+  arr.map((a) => {
+    arrayInJson[a] = Math.cbrt(a);
+  });
+  return arrayInJson;
+};
 
 export { arrayCubeRootToJson };
+
+// console.log(arrayCubeRootToJson([27, 64, 125]));
+// { '27': 3, '64': 4, '125': 5 }
+// console.log(arrayCubeRootToJson(["27", "64", "125", 1]));
+// { '1': 1, '27': 3, '64': 4, '125': 5 }
+// console.log(arrayCubeRootToJson([-1, 1, Infinity, 64, -64]));
+// { '1': 1, '64': 4, '-1': -1, Infinity: Infinity, '-64': -4 }
