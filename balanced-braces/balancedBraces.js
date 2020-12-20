15d03356014d3c8138d4a61fc0abfe412b4cbf1e
rio14 <riteshsinha14.rs@gmail.com>
2020-12-20 23:11:36 +0530

    balanced braces completed

--- balanced-braces/balancedBraces.js
+++ balanced-braces/balancedBraces.js
@@ -1,8 +1,48 @@
+function balancedBraces(str) {
+  const opening = ["[", "{", "("];
+  const closing = ["]", "}", ")"];
+  const braces = { "[": "]", "(": ")", "{": "}" };
+  let temp = [];
+  let i = 0;
+  while (i < str.length) {
+    const cur = str[i];
+    if (opening.includes(str[i])) {
+      temp.push(str[i]);
+    } else if (closing.includes(str[i])) {
+      let last = temp[temp.length - 1];
+      cur === braces[last] ? temp.pop() : temp.push(cur);
+    }
+    i++;
+  }
 
-function balancedBraces(...args) {
-  return args;
+  return temp.length === 0 ? true : false;
 }
 
-export {
-  balancedBraces,
-};
+export { balancedBraces };
+
+console.log(
+  balancedBraces("for(i=0;i<x[i];i++){if(x[i]<10){b++;}else{b+=10;}}")
+); // true
+console.log(
+  balancedBraces("for(i=0;i<x[i];i++){if(x[i]<10){b++;}else{b+=10;}")
+); // false
+console.log(
+  balancedBraces("for(i=0;i<x[[i];i++){if(x[i]<10){b++;}else{b+=10;}}")
+); // false
+console.log(
+  balancedBraces("for(i=0;i<x[i];i++){if(x[i]<10{b++;}else{b+=10;}}")
+); // false
+console.log(
+  balancedBraces("for[(i=0;i<xi];i++){if(x[i]<10){b++;}else{b+=10;}}")
+); // false
+console.log(
+  balancedBraces("for(i=0;i<x[i];i++)({ifx[i]<10){b++;}else{b+=10;}}")
+); // false
+
+console.log(balancedBraces("{}")); // true
+console.log(balancedBraces("{()[{}[]]}")); // true
+console.log(balancedBraces("{(})")); //false
+console.log(balancedBraces("{()[}[]]}")); //false
+console.log(balancedBraces("if(a==b) x = y;")); // true
+console.log(balancedBraces("if(a==b x = y;")); //false
+console.log(balancedBraces("if(x<10}(b++;}else{b+=10;}")); //false
